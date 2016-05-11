b792f0275df39207fae6e478cb2722f51974bf43
mdembele <moussa.dembele.84@gmail.com>
2016-05-11 19:12:18 +0200

    v7

--- launch-admin-training-cluster.sh
+++ launch-admin-training-cluster.sh
@@ -64,7 +64,7 @@ create_persistent_data_disks() {
 			| grep -q $diskname
 
 		if [ $? -eq 0 ] ; then
-			pdisk_args=${pdisk_args}' '' 'name=$diskname' 'mode=rw
+			pdisk_args=${pdisk_args}' '' 'name=$diskname,mode=rw
 		else
 			gcloud compute disks create \
 				$diskname \
